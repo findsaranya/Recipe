d32d0b7aa19661a303b53f2db48f4fa9bac453fd
findsaranya <79015811+findsaranya@users.noreply.github.com>
2021-07-07 11:16:22 +0530

    viewchild added to the form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+ @ViewChild('amountInput') amount:ElementRef;
+ @ViewChild('inputText') ingredientName :ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSubmit(el1:HTMLInputElement,el2:HTMLInputElement){
+    console.log(el1.value,el2.value);
+    console.log(this.amount.nativeElement.value,this.ingredientName.nativeElement.value);
   }
 
 }
