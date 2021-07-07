fae6d77dd21a723f42628cd03ac30dbcf8bd9f3e
findsaranya <79015811+findsaranya@users.noreply.github.com>
2021-07-07 13:09:46 +0530

    Update dropdown.directive.ts

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -4,12 +4,26 @@ import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  @HostListener("click",['$event']) onClick(eventData:Event){
-    //console.log(eventData);
-    let nextSibling = this.renderer.nextSibling(this.eleRef.nativeElement);
-    let parentNode = this.renderer.parentNode(this.eleRef.nativeElement)
-    console.log(parentNode,nextSibling);
-    this.eleRef.nativeElement.nextSibling.classList.toggle('show')
+  // @HostListener("click",['$event']) onClick(eventData:Event){
+  //   //console.log(eventData);
+  //   let nextSibling = this.renderer.nextSibling(this.eleRef.nativeElement);
+  //   let parentNode = this.renderer.parentNode(this.eleRef.nativeElement)
+  //   console.log(parentNode,nextSibling);
+  //   this.eleRef.nativeElement.nextSibling.classList.toggle('show')
+  // }
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    //console.log(this.eleRef.nativeElement);
+    let classNames = this.eleRef.nativeElement.nextSibling.className;
+    console.log(classNames); 
+     if(this.eleRef.nativeElement.contains(event.target)){
+      this.eleRef.nativeElement.nextSibling.classList.toggle('show')
+     }else{
+       if(classNames.includes('show')){
+        this.eleRef.nativeElement.nextSibling.classList.remove('show')
+       }
+     
+     }
+    //this.isOpen =  ? !this.isOpen : false;
   }
   constructor(private eleRef:ElementRef,private renderer:Renderer2) { }
 
