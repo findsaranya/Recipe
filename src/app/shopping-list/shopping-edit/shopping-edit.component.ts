aea53a67c2614f5f3eec7a6eade60d09a0993829
findsaranya <79015811+findsaranya@users.noreply.github.com>
2021-07-08 16:00:46 +0530

    form validation

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Recipe } from 'src/app/recipes/recipe.model';
 import { Ingredient } from 'src/app/shared/ingredients.model';
 import { ShoppingService } from '../shopping.service';
@@ -9,21 +10,24 @@ import { ShoppingService } from '../shopping.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
- @ViewChild('amountInput') amount:ElementRef;
- @ViewChild('ingText') ingredientName :ElementRef;
- //@Output() addIngredient = new EventEmitter<Ingredient>();
+ 
+ @ViewChild('form')shopping:NgForm;
+ 
   constructor(private shoppingService:ShoppingService) { }
 
   ngOnInit(): void {
   }
   onSubmit(){
+    //console.log(this.shopping.value);
+    let {name,amount}  = this.shopping.value;
+    
     //console.log(el1.value,el2.value);
-    console.log(typeof this.amount.nativeElement.value);
-    const newIngredient = new Ingredient(
-      this.ingredientName.nativeElement.value,
-      this.amount.nativeElement.value
-      )
-      this.shoppingService.addNewIngredients(newIngredient);
+    // console.log(typeof this.amount.nativeElement.value);
+     const newIngredient = new Ingredient(
+       name,amount
+       )
+        this.shoppingService.addNewIngredients(newIngredient);
+        this.shopping.reset();
     //this.addIngredient.emit(newIngredient);
 
   }
